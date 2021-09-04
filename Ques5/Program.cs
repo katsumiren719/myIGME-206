81f5324c9d7a6a3fa24264ea28c8b175c38fdf39
Raj Barot <rb9769@g.rit.edu>
2021-09-03 23:31:58 -0400

    final

---
+++ Ques5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ques5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number : ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number : ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the third number : ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the forth number : ");
+            double d = Convert.ToDouble(Console.ReadLine());
+
+            double e = a * b * c * d;
+            Console.WriteLine("The product of all 4 numbers are : " + e);
+        }
+    }
+}
