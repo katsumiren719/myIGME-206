dcf93f0fb7394d50fee6591d6bbb334b599a7a01
Raj Barot <rb9769@g.rit.edu>
2021-09-15 23:37:19 -0400

    couldnt do it

---
+++ Pe92/Program.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Pe92
+{
+
+    //Author : raj barot
+       // Aim : to add 5 seconds per question and wrong answer if they cannot answer it in that time
+       // i understood the thread part but didnt figure out a way to add this for 5 seconds hence i commented that block and my thread creeation code as well.
+
+     class Program
+    {
+        static void Main()
+        {
+            // store user name
+            string myName = "";
+
+            // string and int of # of questions
+            string sQuestions = "";
+            int nQuestions = 0;
+
+            // string and base value related to difficulty
+            string sDifficulty = "";
+            int nMaxRange = 0;
+
+            // constant for setting difficulty with 1 variable
+            const int MAX_BASE = 10;
+
+            // question and # correct counters
+            int nCntr = 0;
+            int nCorrect = 0;
+
+            // operator picker
+            int nOp = 0;
+
+            // operands and solution
+            int val1 = 0;
+            int val2 = 0;
+            int nAnswer = 0;
+
+            // string and int for the response
+            string sResponse = "";
+            Int32 nResponse = 0;
+
+            // boolean for checking valid input
+            bool bValid = false;
+
+            // play again?
+            string sAgain = "";
+
+            // seed the random number generator
+            Random rand = new Random();
+
+
+             Timer timeOutTimer;
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Math Quiz!");
+            Console.WriteLine();
+
+            // fetch the user's name into myName
+            while (true)
+            {
+                Console.Write("What is your name-> ");
+                myName = Console.ReadLine();
+
+                if (myName.Length > 0)
+                {
+                    break;
+                }
+            }
+
+        // label to return to if they want to play again
+        start:
+
+            // initialize correct responses for each time around
+            nCorrect = 0;
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("How many questions-> ");
+                sQuestions = Console.ReadLine();
+
+                try
+                {
+                    nQuestions = int.Parse(sQuestions);
+                    bValid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer.");
+                    bValid = false;
+                }
+
+            } while (!bValid);
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Difficulty level (easy, medium, hard)-> ");
+                sDifficulty = Console.ReadLine();
+            } while (sDifficulty.ToLower() != "easy" &&
+                     sDifficulty.ToLower() != "medium" &&
+                     sDifficulty.ToLower() != "hard");
+
+            Console.WriteLine();
+
+            // if they choose easy, then set nMaxRange = MAX_BASE, unless myName == "David", then set difficulty to hard
+            // if they choose medium, set nMaxRange = MAX_BASE * 2
+            // if they choose hard, set nMaxRange = MAX_BASE * 3
+            switch (sDifficulty.ToLower())
+            {
+                case "easy":
+                    nMaxRange = MAX_BASE;
+                    if (myName.ToLower() == "david")
+                    {
+                        goto case "hard";
+                    }
+                    break;
+
+                case "medium":
+                    nMaxRange = MAX_BASE * 2;
+                    break;
+
+                case "hard":
+                    nMaxRange = MAX_BASE * 3;
+                    break;
+            }
+
+            // ask each question
+            for (nCntr = 0; nCntr < nQuestions; ++nCntr)
+            {
+                // generate a random number between 0 inclusive and 3 exclusive to get the operation
+                nOp = rand.Next(0, 3);
+
+                val1 = rand.Next(0, nMaxRange) + nMaxRange;
+                val2 = rand.Next(0, nMaxRange);
+
+                // if either argument is 0, pick new numbers
+                if (val1 == 0 || val2 == 0)
+                {
+                    // decrement counter to try this one again (because it will be incremented at the top of the loop)
+                    --nCntr;
+                    continue;
+                }
+
+                // if nOp == 0, then addition
+                // if nOp == 1, then subtraction
+                // else multiplication
+                if (nOp == 0)
+                {
+                    nAnswer = val1 + val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} + {val2} => ";
+                }
+                else if (nOp == 1)
+                {
+                    nAnswer = val1 - val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} - {val2} => ";
+                }
+                else
+                {
+                    nAnswer = val1 * val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} * {val2} => ";
+                }
+
+
+
+
+
+
+
+
+                /*
+             
+
+                Console.Write(sQuestions);
+                sResponse = Console.ReadLine();
+
+                timeOutTimer = new Timer(5000);   // 5 seconds
+                ElapsedEventHandler elapsedEventHandler;
+                elapsedEventHandler = new ElapsedEventHandler(TimesUp);
+                timeOutTimer.Elapsed += elapsedEventHandler;
+                timeOutTimer.Start();
+
+                void TimesUp(object source, ElapsedEventArgs e)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Your time is up!");
+                     bool bTimeOut = false;
+                    //  set the bTimeOut flag to quit the game
+                    bTimeOut = true;
+
+                    //  stop the timeOutTimer
+                    timeOutTimer.Stop()
+                }
+
+
+               */
+                // this was the part i could do understand 
+
+
+
+                // but couldnt part a way to figure out a way to merge this code for 5 sec wait
+                /*if (sAnswer.ToUpper() == displayString && !bTimeOut)
+                {
+                    // 8. congratulate and write their current score (displayString.Length)
+                    Console.WriteLine("Well Done! Your current score is {0}", );
+                }
+                else
+                {
+                    // 9. otherwise display the correct code sequence and their final score (displayString.Length - 1)
+                    Console.WriteLine("Bad luck. :( The correct code was {0}.  Your final score is: {1}", displayString, displayString.Length - 1);
+
+                    // 10. set bTimeOut to true to exit the game loop
+                    bTimeOut = true;
+                }*/
+
+
+
+
+                do
+                {
+                    
+                    
+                    try
+                    {
+                        nResponse = int.Parse(sResponse);
+                        bValid = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter an integer.");
+                        bValid = false;
+                    }
+
+                    
+
+                } while (!bValid);
+
+                // if response == answer, output flashy reward and increment # correct
+                // else output stark answer
+                if (nResponse == nAnswer)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Well done, {0}!!!", myName);
+
+                    ++nCorrect;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("I'm sorry {0}. The answer is {1}", myName, nAnswer);
+                }
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            // output how many they got correct and their score
+            Console.WriteLine("You got {0} correct out of {1}, which is a score of {2:P2}", nCorrect, nQuestions, Convert.ToDouble(nCorrect) / (double)nCntr);
+            Console.WriteLine();
+
+            do
+            {
+                // prompt if they want to play again
+                Console.Write("Do you want to play again? ");
+
+                sAgain = Console.ReadLine();
+
+                if (sAgain.ToLower().StartsWith("y"))
+                {
+                    goto start;
+                }
+                else if (sAgain.ToLower().StartsWith("n"))
+                {
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
---
+++ demo2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo2
+{
+    struct order
+    {
+        public string itemName;
+        public int unitCount;
+        public double unitCost;
+        public double TotalCost() => unitCount * unitCost;
+        public string Info() => "Order information: " + unitCount.ToString() +
+        " " + itemName + " items at $" + unitCost.ToString() +
+        " each, total cost $" + TotalCost().ToString();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            order o;
+            o.itemName = "lala";
+            o.unitCount = 5;
+            o.unitCost = 5.0;
+            o.Info();
+
+        }
+    }
+}
