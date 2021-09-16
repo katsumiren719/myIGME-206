42cc1583c7311873b42d841680e776bf828f2a95
Raj Barot <rb9769@g.rit.edu>
2021-09-15 21:18:49 -0400

    final

---
+++ PE_9_3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_9_3
+{
+    class Program
+    {
+        delegate string delegateReadLine();
+        static void Main(string[] args)
+        {
+            WriteLine("Type something :");
+            delegateReadLine readLine = new delegateReadLine(ReadLine);
+            
+            string userInput = readLine();
+            WriteLine($"Your String: {userInput}");
+
+        }
+    }
+}
+
