754a4ffbfce87c1253b72a7cd58d74909e30cf65
Raj Barot <rb9769@g.rit.edu>
2021-09-26 18:35:07 -0400

    final

---
+++ UT_13/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace UT_13
+{
+    // Author :- RAJ BAROT
+    /* Purpose :- The function should increase the salary by $19,999.99 if name = your name and return true
+    Otherwise it should return false.
+    The main program should congratulate the user if they got a raise, and display their new salary.*/
+
+    struct employee
+    {
+        // Using structure to do the same thing as Q12
+        public string sName;  // need them to be public variables else we cannot access them outside
+        public double dSalary;
+    };
+
+     class Program
+    {
+        // Author :- RAJ BAROT
+        // Using structure to do the same thing as Q12
+
+        static void Main(string[] args)
+        {
+            employee e1;
+            e1.dSalary = 30000;
+            // initialising variables 
+            Console.WriteLine("Enter Your Name");
+            e1.sName = Console.ReadLine();
+            if (GiveRaise(e1) == true) ; // passing struct object to give raise method
+            {
+                Console.WriteLine("Congratulations, your salary got increase");
+                Console.WriteLine("This is your New Salary " + e1.dSalary);
+            }
+            
+        }
+
+        static bool GiveRaise(employee e)
+        // Function to increase Salary
+        {
+            if (e.sName == "RAJ")
+            {
+                e.dSalary = e.dSalary + 19999.99;  // if conditin = true increase salary
+            }
+            return true;
+        }
+
+    
+}
+}
+    
+
+
+
+
+        
