4e813ecdc3c36f70f797338465d6e5a218c7e3bc
Raj Barot <rb9769@g.rit.edu>
2021-10-01 19:53:41 -0400

    FINAL

---
+++ PE12_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyClass;
+
+namespace PE12_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass obj1 = new MyDerivedClass();
+            obj1.MyString = "NEWSTRING";
+            Console.WriteLine(obj1.GetString());
+        }
+    }
+}
