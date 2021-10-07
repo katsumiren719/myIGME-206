f8d79752ad87c5953b9e96b0a06065c026430522
Raj Barot <rb9769@g.rit.edu>
2021-10-06 21:51:40 -0400

    final

---
+++ pe143.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Animals;
+
+namespace MyApp
+{
+    //this program tests Animals
+    class Program
+    {
+        //main method
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Cat c1 = new Cat();
+
+            Console.WriteLine("Dog eats food like:");
+            MyMethod(d1);
+
+            Console.WriteLine("Cat eats food like");
+            MyMethod(c1);
+        }
+
+        public static void MyMethod(object myObject)
+        {
+            PetEat newobject = (PetEat)myObject;
+            newobject.Eat();
+        }
+    }
+
+
+}
