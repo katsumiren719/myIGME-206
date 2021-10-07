e1706b6223af76fb8dee9bbd241c067d520e4a81
Raj Barot <rb9769@g.rit.edu>
2021-10-06 21:44:00 -0400

    final

---
+++ Animals/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Pets
+    {
+        private string petname;
+
+        public string petrwmethod
+        {
+            get => petname;
+            set => petname = value;
+        }
+    }
+
+    public interface PetEat
+    {
+        void Eat();
+    }
+
+    public class Dog : PetEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eat Happy");
+        }
+    }
+
+    public class Cat : PetEat
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Spill the Food");
+        }
+    }
+}
---
+++ pe14/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pe14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
