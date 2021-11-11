5cc66f8943c3cf10b245742430673038ad2b3021
Raj Barot <rb9769@g.rit.edu>
2021-11-10 23:04:49 -0500

    ive no clue how to do this one

---
+++ PE 22/PE 22/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PE_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
