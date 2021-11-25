43c2aa443b483eb6ec5f141af73296305978e90b
Raj Barot <rb9769@g.rit.edu>
2021-11-25 00:04:26 -0500

    ok

---
+++ ut3q7/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT3Q7
+{
+    // Author:- Raj Barot
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Wizard> wizards = new List<Wizard>
+            {
+                new Wizard("raj", 21),
+                new Wizard("apple", 21),
+                new Wizard("banana", 19),
+                new Wizard("carot", 20),
+                new Wizard("cabage", 23),
+                new Wizard("potato", 21),
+                new Wizard("lufy", 19),
+                new Wizard("leafy", 21),
+                new Wizard("lify", 19),
+                new Wizard("lofy", 23),
+            };
+            Console.WriteLine("Before sorting:\n");
+            foreach (var wizard in wizards)
+            {
+                Console.WriteLine("Name: {0}, Age: {1}", wizard.name, wizard.age);
+            }
+            wizards.Sort(delegate (Wizard x, Wizard y)
+            {
+                return x.CompareTo(y);
+            });
+            Console.WriteLine("After sorting:\n");
+            foreach (var wizard in wizards)
+            {
+                Console.WriteLine("Name: {0}, Age: {1}", wizard.name, wizard.age);
+            }
+        }
+    }
+    class Wizard
+    {
+        public string name;
+        public int age;
+        public Wizard(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public int CompareTo(Wizard another)
+        {
+            if (this.age > another.age)
+            {
+                return 1;
+            }
+            else if (this.age == another.age)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
